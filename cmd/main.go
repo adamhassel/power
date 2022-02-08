9838e86e004073530870cb9a41bfcd15ef8c1227
Adam Hasselbalch Hansen <adam@hasselbalch.com>
2022-02-08 09:26:36 +0100

    Add -s (simple) output option

--- cmd/main.go
+++ cmd/main.go
@@ -15,12 +15,13 @@ import (
 
 var confFile string
 var noOfHours uint
-var pretty bool
+var pretty, simple bool
 
 func init() {
 	flag.UintVar(&noOfHours, "h", 12, "Number of hours to get price data for.")
 	flag.StringVar(&confFile, "c", "power.conf", "location of configuration file.")
 	flag.BoolVar(&pretty, "p", false, "pretty-print (indent) JSON output.")
+	flag.BoolVar(&simple, "s", false, "simple data output, only period and total price.")
 }
 func main() {
 	flag.Parse()
@@ -51,14 +52,30 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	var combined interface{}
+	combined = power.Summarize(p.(energidataservice.Prices), ft.(eloverblik.FullTariffs))
 
-	combined := power.Summarize(p.(energidataservice.Prices), ft.(eloverblik.FullTariffs))
+	if simple {
+		type Simple struct {
+			Period string `json:"period"`
+			Price  string `json:"price"`
+		}
+		data := combined.([]entities.FullPrice)
+		o := make([]Simple, len(data))
+		for i, p := range data {
+			o[i].Period = fmt.Sprintf("%s - %s", p.ValidFrom.Format("15:04"), p.ValidTo.Format("15:04"))
+			o[i].Price = fmt.Sprintf("%0.2f kr.", p.TotalIncVAT)
+		}
+		combined = o
+	}
+
 	var output []byte
 	if pretty {
 		output, err = json.MarshalIndent(combined, "", "  ")
 	} else {
 		output, err = json.Marshal(combined)
 	}
+
 	if err != nil {
 		log.Fatalf("error marshalling result: %s", err)
 	}
